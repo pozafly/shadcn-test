11ffe220dab45b54c5cd11452b6be26a7e64077a
pozafly <pozafly@gmail.com>
2024-10-05 19:21:40 +0900

    feat: add theme toggler

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { useState } from 'react';
-import './App.css';
 import { Button } from '@/components/ui/button';
+import { ThemeProvider } from './components/provider/ThemeProvider';
+import ThemeToggler from './components/custom/ThemeToggler';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <Button onClick={() => setCount((count) => count + 1)}>Click me</Button>
-      <p>You clicked {count} times</p>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="dark:bg-zinc-950 w-screen h-screen">
+        <ThemeToggler />
+
+        <div className="mt-16">
+          <Button onClick={() => setCount((count) => count + 1)}>
+            Click me
+          </Button>
+          <p>You clicked {count} times</p>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
