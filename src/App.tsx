f2b73625447b7ba19b7f38a35704c3f19f873bf8
pozafly <pozafly@gmail.com>
2024-10-05 11:05:11 +0900

    feat: add first button

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useState } from 'react';
 import './App.css';
+import { Button } from '@/components/ui/button';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h1 className="text-3xl font-bold">Hello world!</h1>
-    </>
+    <div>
+      <Button onClick={() => setCount((count) => count + 1)}>Click me</Button>
+      <p>You clicked {count} times</p>
+    </div>
   );
 }
 
